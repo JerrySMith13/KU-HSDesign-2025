ec80ca717304e984e8b4e5a35d919f66f918fbab
Jeremy  Smith <jeremy.ws.2008@gmail.com>
2025-09-05 12:26:11 -0500

    Re-initialized frontend

---
+++ KU-HSDesign-Frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { useRef, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import Toolbar from './Toolbar/Toolbar'
+function App() {
+
+  return (
+    <>
+      <Toolbar />
+    </>
+  )
+}
+
+export default App
